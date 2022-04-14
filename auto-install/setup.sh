bd3f6a564ab06a0590148614b8119865fbb3028c
Rob Taylor <manix84@gmail.com>
2022-04-14 18:14:22 +0100

    Update setup.sh

--- auto-install/setup.sh
+++ auto-install/setup.sh
@@ -45,7 +45,7 @@ sudo apt install -y -qq zsh git neofetch wget
 ###Install OhMyZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &
 INSTALL_PID=$!
-wait INSTALL_PID
+wait $INSTALL_PID
 
 ### Install OhMyZSH-BulletTrain
 wget --output-document=$ZSH_CUSTOM/themes/bullet-train.zsh-theme http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
