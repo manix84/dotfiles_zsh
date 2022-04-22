a1e61e2d65c9671e65d2239c27f44252f41f03a9
Rob Taylor <manix84@gmail.com>
2022-04-22 13:39:57 +0100

    Update setup.sh

--- auto-install/setup.sh
+++ auto-install/setup.sh
@@ -50,6 +50,7 @@ INSTALL_PID=$!
 wait $INSTALL_PID
 
 ### Install OhMyZSH-BulletTrain
+export ZSH_CUSTOM=~/.oh-my-zsh/custom
 wget --output-document=$ZSH_CUSTOM/themes/bullet-train.zsh-theme http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
 sed -i".backup" 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bullet-train"/g' ~/.zshrc
 
