20434d6ef508b4c0c6452b69a1ff2e514f8f0c27
Rob Taylor <373278+manix84@users.noreply.github.com>
2025-04-25 09:27:24 +0000

    Update setup-user.sh

--- auto-install/setup-user.sh
+++ auto-install/setup-user.sh
@@ -1,39 +1,123 @@
-###Install OhMyZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &
-INSTALL_PID=$!
-wait $INSTALL_PID
+#!/bin/bash
 
-### Install OhMyZSH-BulletTrain
-export ZSH_CUSTOM=~/.oh-my-zsh/custom
-wget --output-document=$ZSH_CUSTOM/themes/bullet-train.zsh-theme http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
-cp ~/.zshrc ~/.zshrc.backup
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bullet-train"/g' ~/.zshrc
-echo 'ENABLE_CORRECTION="true"' >>  ~/.zshrc
-echo 'DISABLE_UPDATE_PROMPT="true"' >> ~/.zshrc
-echo 'DISABLE_AUTO_UPDATE="false"' >> ~/.zshrc
+set -eu
+IFS=$'\n\t'
 
-### Install MOTD - Neofetch
-echo "printf '\033[2J'\nneofetch" > ~/.motd
-echo "\nif [ -f ~/.motd ]; then\n  source ~/.motd\nfi" >> ~/.zshrc
-chown 0700 ~/.motd
+: <<'DISCLAIMER'
 
-### Setup Neofetch ###
-# cp ~/.config/neofetch/config.conf ~/.config/neofetch/config.conf.backup
-# sed -i 's/# info "Local IP"/info "Local IP"/g' ~/.config/neofetch/config.conf
-# sed -i 's/# info "Public IP"/info "Public IP"/g' ~/.config/neofetch/config.conf
-# sed -i 's/# info "CPU Usage"/info "CPU Usage"/g' ~/.config/neofetch/config.conf
-# sed -i 's/# info "Disk"/info "Disk"/g' ~/.config/neofetch/config.conf
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
 
-### Setup FastFetch ###
-# mkdir -p ~/.config/fastfetch/
-# wget --output-document=~/.config/fastfetch/config.jsonc https://raw.githubusercontent.com/manix84/dotfiles_zsh/refs/heads/main/.config/fastfetch/config.jsonc
+This script is licensed under the terms of the MIT license.
+Unless otherwise noted, code reproduced herein
+was written for this script.
 
-### Setup Nano ###
-curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
-cat /etc/nanorc >> ~/.nanorc
+- Manix84 -
 
-### Change shell to ZSH
-chsh -s "$(which zsh)"
+DISCLAIMER
 
-### Switch to ZSH
+# === Logging ===
+LOGFILE=~/setup-$(date +%Y%m%d%H%M).log
+exec > >(tee -a "$LOGFILE") 2>&1
+
+# === Globals ===
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+# === Helpers ===
+detect_platform_arch() {
+  local os="$(uname -s)"
+  local arch="$(uname -m)"
+
+  case "$os" in
+    Darwin)
+      echo "macos-universal";;
+    Linux)
+      case "$arch" in
+        aarch64) echo "linux-aarch64";;
+        armv6l) echo "linux-armv6l";;
+        armv7l) echo "linux-armv7l";;
+        x86_64) echo "linux-amd64";;
+        ppc64le|riscv64|s390x) echo "linux-$arch";;
+        i386|i686) echo "sunos-i386";;
+        *) echo "unsupported";;
+      esac;;
+    *) echo "unsupported";;
+  esac
+}
+
+download_file() {
+  local url="" output=""
+  while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+      --output=*) output="${1#--output=}";;
+      --output) output="$2"; shift;;
+      *) url="$1";;
+    esac
+    shift
+  done
+
+  [[ -z "$url" ]] && { echo "No URL provided." >&2; return 1; }
+
+  if command -v wget &>/dev/null; then wget -O "$output" "$url"
+  elif command -v curl &>/dev/null; then curl -Lo "$output" "$url"
+  else echo "No download utility found." >&2; return 1
+  fi
+}
+
+execute_online_script() {
+  local url="$1"
+  if command -v curl >/dev/null; then bash -c "$(curl -fsSL $url)"
+  elif command -v wget >/dev/null; then bash -c "$(wget -qO- $url)"
+  else echo "No supported downloader found." >&2; return 1
+  fi
+}
+
+append_to_zshrc() {
+  local line="$1"
+  grep -qxF "$line" ~/.zshrc || echo "$line" >> ~/.zshrc
+}
+
+install_oh_my_zsh() {
+  RUNZSH=no KEEP_ZSHRC=yes execute_online_script https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+  download_file http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme --output=$ZSH_CUSTOM/themes/bullet-train.zsh-theme
+
+  [[ -f ~/.zshrc ]] && cp ~/.zshrc ~/.zshrc.backup
+  sed -i.bak 's/ZSH_THEME=\"[^"]*\"/ZSH_THEME=\"bullet-train\"/' ~/.zshrc
+
+  append_to_zshrc 'ENABLE_CORRECTION="true"'
+  append_to_zshrc 'DISABLE_UPDATE_PROMPT="true"'
+  append_to_zshrc 'DISABLE_AUTO_UPDATE="false"'
+
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions || true
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting || true
+  append_to_zshrc 'plugins=(git z zsh-autosuggestions zsh-syntax-highlighting)'
+}
+
+install_motd() {
+  echo -e "printf '\033[2J'\nfastfetch" > ~/.motd
+  append_to_zshrc "[[ -f ~/.motd ]] && source ~/.motd"
+  chmod 0700 ~/.motd
+}
+
+install_nano_highlight() {
+  execute_online_script https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh
+  [[ -f /etc/nanorc ]] && cat /etc/nanorc >> ~/.nanorc
+}
+
+change_shell_to_zsh() {
+  [[ $SHELL != *zsh ]] && chsh -s "$(which zsh)"
+}
+
+# === Main Install Steps ===
+install_oh_my_zsh
+install_motd
+install_nano_highlight
+change_shell_to_zsh
+
 zsh
